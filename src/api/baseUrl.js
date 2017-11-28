d69d49971ad2a600475e3dd5c2e92042c0336639
Brett Jensen <brett@adduco.cdigital>
2017-11-28 19:26:43 +0000

    Add bundle splitting and cache busting to project

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,8 +1,13 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:2881' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:2881/' : '/';
 }
 
 function getQueryStringParameterByName(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "")
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if(!results) return null;
+    if(!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
